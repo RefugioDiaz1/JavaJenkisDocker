9b7f4e28e5a56cdc4a4c092b596db6ec06cb4a56
refugiodiaz1 <refugiodelaodiaz@gmail.com>
2021-11-30 18:51:44 -0600

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+        public static void main(String[] args){
+            System.out.println(checkIfInputIsAnEvenNumber(123));
+
+        }
+        public static boolean checkIfInputIsAnEvenNumber(int number){
+            return number % 2 ==0;
+
+        }
+
+
+    }
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TestMain {
+
+    @Test
+    public  void testInputIsEven(){
+        Assertions.assertTrue(Main.checkIfInputIsAnEvenNumber(22)); //Assertion
+
+    }
+
+}
